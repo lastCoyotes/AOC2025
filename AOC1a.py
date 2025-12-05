0f3ce4719162e3b74d5e7480044c2e43413c71d7
lastCoyotes <lastcoyotes@gmail.com>
2025-12-05 07:50:21 -0800

    python solution for day 1 a

---
+++ AOC1a.py
@@ -0,0 +1,17 @@
+input = open('input/1a.txt', 'r')
+
+result: int = 0
+position: int = 50
+
+for line in input:
+    line = line.strip()
+
+    if line[0] == 'R':
+        position += int(line[1:])
+    if line[0] == 'L':
+        position -= int(line[1:])
+    position = position % 100
+    if position == 0:
+        result += 1
+
+print(result)
